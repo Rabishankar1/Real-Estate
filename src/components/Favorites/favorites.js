decc08ca1d1660dc8eb78b68786cba1baacdc444
Rabishankar Panigrahi <rabishankar.panigrahi14@gmail.com>
2022-10-02 11:24:07 +0530

    minor changes

--- src/components/Favorites/favorites.js
+++ src/components/Favorites/favorites.js
@@ -3,19 +3,32 @@ import Content from '../content/content';
 import "./favorites.css";
 
 function Favorites(props) {
-    const data = JSON.parse(localStorage.getItem('info'));
+    let data;
+    if (JSON.parse(localStorage.getItem('info'))) {
+        data = JSON.parse(localStorage.getItem('info'));
+    }
+    else {
+        data = []
+    }
     return (
         <>
             {props.state && <div className='space'></div>}
             <div className="favorite-wrap">
                 <div className='grid-container'>
-                    {data.map(i => {
+                    {data && (data.map(i => {
                         return (
                             <div className="grid-item">
                                 <Content data={i} />
                             </div>)
-                    })}
+                    }))}
                 </div>
+                {!data.length && (
+                    <>
+                        <h1 style={{ color: "rgba(0, 0, 0, 0.6)", margin: "auto" }}>Favorites that get added will appear here</h1>
+                        <br />
+                        <h1 style={{ color: "rgba(0, 0, 0, 0.6)", margin: "auto" }}> Go to home page and add a property to see it</h1>
+                    </>
+                )}
             </div>
         </>
     )
